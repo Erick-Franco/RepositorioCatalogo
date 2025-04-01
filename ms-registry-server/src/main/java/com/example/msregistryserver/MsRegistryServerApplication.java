395ad7ee27484b2e23c7ca161cf335f62e322a3b
Erick-Franco <rrickquispe@gmail.com>
2025-04-01 17:27:55 -0500

    registry

--- ms-registry-server/src/main/java/com/example/msregistryserver/MsRegistryServerApplication.java
+++ ms-registry-server/src/main/java/com/example/msregistryserver/MsRegistryServerApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
 @SpringBootApplication
 @EnableEurekaServer
 public class MsRegistryServerApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(MsRegistryServerApplication.class, args);
     }
+
 
 }
