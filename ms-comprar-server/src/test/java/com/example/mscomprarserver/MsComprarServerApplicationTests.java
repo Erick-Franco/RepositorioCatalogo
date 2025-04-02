fb2018b7026f17503bf79402bdbb93578567fded
Erick-Franco <rrickquispe@gmail.com>
2025-04-02 16:09:15 -0500

    Compraserver

---
+++ ms-comprar-server/src/test/java/com/example/mscomprarserver/MsComprarServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mscomprarserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsComprarServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
